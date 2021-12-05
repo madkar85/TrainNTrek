92c36dda2914468dd14022e72587548571d3c703
farzane-zafar <74569907+farzane-zafar@users.noreply.github.com>
2021-12-05 20:44:33 +0100

    Add CSS-file to BMI calculator component

--- src/components/bmiCalculator/BMICalculator.js
+++ src/components/bmiCalculator/BMICalculator.js
@@ -1,42 +1,62 @@
 import React, { useState } from "react";
+import "./BMICalculator.css"
 
 const BMICalculator = () => {
+  const [text, settext] = useState();
   const [bmi, setBmi] = useState();
   const [info, setInfo] = useState();
+  const [infoText, setInfoText] = useState();
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState();
+
   const handleBmi = () => {
     let val = (
       [Number(weight) / Number(height) / Number(height)] * 10000
     ).toFixed(1);
     setBmi(val);
+    settext("Your BMI is: ");
     if (val < 18.5) {
       setInfo("Under Weight");
+      setInfoText("Being underweight could be a sign you're not eating enough or you may be ill.");
     } else if (val > 18.5 && val <= 24.9) {
-      setInfo("Healthy");
+      setInfo("Healthy weight");
+      setInfoText("Keep up the good work! For tips on maintaining a healthy weight, check out the diet and fitness sections.");
     } else if (val > 24.9 && val < 30) {
       setInfo("Overweight");
+      setInfoText("The best way to lose weight if you're overweight is through a combination of diet and exercise.");
     } else {
       setInfo("Obese");
+      setInfoText("The best way to lose weight if you're obese is through a combination of diet and exercise, and, in some cases, medicines.");
     }
   };
+
   return (
-    <div>
-      
-      <input
+    <div className="container">
+    <div className="form-control">
+      <div className="signin-text"><h2>BMI-Calculatore</h2></div>
+        <div className="signin-text"> Modify the values and ckick the Calculate button to use.</div>
+        <input 
         type="text"
         onChange={(e) => setHeight(e.target.value)}
-        placeholder="height in cm"
-      />
-      <input
+        placeholder=" Enter you height in cm"
+         />
+    </div>
+    <div className="form-control">
+        <input 
         type="text"
         onChange={(e) => setWeight(e.target.value)}
-        placeholder="Weight in kg"
-      />
-      <button onClick={handleBmi}>Calculate</button>
-      <h1>{bmi}</h1>
-      <h2>{info}</h2>
+        placeholder=" Enter your weight in kg"
+        />
     </div>
+    <button className="btn btn-block" onClick={handleBmi} > Calculate </button>
+    <div>
+    <h1 className="bmi-text">{text} {bmi}</h1>
+    <h3 className="bmi-text">{info}</h3>
+    <div className="bmi-text">{infoText}</div>
+    
+    </div>
+
+</div>
   );
 };
 
--- src/view/bmiview/BMIView.js
+++ src/view/bmiview/BMIView.js
@@ -10,7 +10,6 @@ const BMIView = () => {
         <>
             <div className="bmiView">
             <section className="textField">
-                    <h1> Modify the values and ckick the Calculate button to use</h1>
                     <BMICalculator />
                 </section>
                 <Footer />
