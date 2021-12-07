ce244e4a395c6f06f43a37e3f38406b09e822703
Dennis Lindquist <dennislindquist@gmail.com>
2021-12-07 22:35:12 +0100

    come on..

--- src/components/dietMeal/Meal.js
+++ src/components/dietMeal/Meal.js
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
-export default function Meal({ meal }) {
-  const [imageUrl, setImageUrl] = useState("");
-  const spoonacularkey = process.env.REACT_APP_SPOONACULAR_KEY;
-
-  useEffect(() => {
-    fetch(
-      `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=6284978e18876d3fb8a708214ce849ad45894d9c&includeNutrition=false`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setImageUrl(data.image);
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  }, [meal.id]);
-
+export default function Table(props) {
   return (
-    <article>
-      <h1>{meal.title}</h1>
-      <img src={imageUrl} alt="recipe" />
-      <ul className="instructions">
-        <li>Preparation time: {meal.readyInMinutes} minutes</li>
-        <li>Number of servings: {meal.servings}</li>
-      </ul>
-
-      <a href={meal.sourceUrl}>Go to Recipe</a>
-    </article>
+    <React.Fragment>
+      <tr>
+        <td>{props.meal.text}</td>
+        <td>{props.meal.calorie}</td>
+        <td>
+          <FaTrash onClick={props.onDelete} className="mr-4" />
+          <FaEdit />
+        </td>
+      </tr>
+    </React.Fragment>
   );
 }
--- src/view/dietview/DietView.js
+++ src/view/dietview/DietView.js
@@ -1,42 +1,65 @@
-import React, { useState } from "react";
-import MealList from "../../components/dietMeal/MealList";
+import React, { Component } from "react";
 import "./DietView.css";
+import Form from "../../components/dietMeal/Form";
+import Meal from "../../components/dietMeal/Meal";
 
-function DietView() {
-  const [mealData, setMealData] = useState(null);
-  const [calories, setCalories] = useState(2000);
-  const spoonacularkey = process.env.REACT_APP_SPOONACULAR_KEY;
+class DietView extends Component {
+  state = {
+    meals: [],
+  };
 
-  function getMealData() {
-    fetch(
-      `https://api.spoonacular.com/mealplanner/generate?apiKey=${spoonacularkey}&timeFrame=day&targetCalories=${calories}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMealData(data);
-      })
-      .catch(() => {
-        console.log("error");
-      });
+  addMeal = (meal) => {
+    this.setState({
+      meals: [meal, ...this.state.meals],
+    });
+  };
+
+  onDelete = (id) => {
+    this.setState({
+      meals: this.state.meals.filter((meal) => meal.id !== id),
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h2>Calorie Counter</h2>
+          <hr />
+          <Form onsubmit={this.addMeal} />
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Meal</th>
+                <th>Calories</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.meals.map((meal) => (
+                <Meal
+                  key={meal.id}
+                  meal={meal}
+                  onDelete={() => this.onDelete(meal.id)}
+                />
+              ))}
+              <tr>
+                <td>Total:</td>
+                <td>
+                  <span role="img">🍎</span>
+                  {this.state.meals.reduce(
+                    (totalCalories, meal) => totalCalories + meal.calorie,
+                    0
+                  )}
+                </td>
+                <td />
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
-
-  function handleChange(e) {
-    setCalories(e.target.value);
-  }
-
-  return (
-    <div className="Diet">
-      <section className="controls">
-        <input
-          type="number"
-          placeholder="Calories (e.g. 2000)"
-          onChange={handleChange}
-        />
-        <button onClick={getMealData}>Get Daily Meal Plan</button>
-      </section>
-      {mealData && <MealList mealData={mealData} />}
-    </div>
-  );
 }
 
 export default DietView;
