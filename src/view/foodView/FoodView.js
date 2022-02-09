d8c075d93abb3b7db37b4f1afe44048e21b1e15c
madkar85 <madeleine.karlsson@outlook.com>
2022-02-09 14:46:44 +0100

    Added more info on FoodView

--- src/view/foodView/FoodView.js
+++ src/view/foodView/FoodView.js
@@ -8,10 +8,20 @@ const FoodView = () => {
     return (
        <div className='foodView'>
             <h1 className='foodHeading'>Food facts and inspiration</h1>
-            <div className='nutritionFacts'>
-                <h2>Nutrition</h2>
+            <p>If your goal is to lose weight, remember that you still need to not only eat healthy but
+            also make sure you eat enough. That means make sure you et enought protein, vitamins,
+            minerals and fats in your diet. This page helps you both with information about nutrition
+                and recipe inspiration</p>
+            <div className='contentLayout'>
+                <div className='nutritionFacts'>
+                    <h2>Nutrition</h2>
+                    <p>Test test</p>
+                </div>
+                <div className='recipes'>
+                    <h2>Links for recipes</h2>
+                    <p>Test test</p>
+                </div>
             </div>
-            
        </div>
       
     )
