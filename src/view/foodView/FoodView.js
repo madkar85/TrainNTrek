6e880b20404f2cd3330f8bed7a1342bf2414fd2b
madkar85 <madeleine.karlsson@outlook.com>
2022-02-09 11:50:26 +0100

    Added headlines and some css in FoodView

--- src/view/foodView/FoodView.js
+++ src/view/foodView/FoodView.js
@@ -1,12 +1,19 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import RoutingPath from "../../routes/RoutingPath"
+import './FoodView.css'
 
 const FoodView = () => {
-
+    const history = useHistory();
 
     return (
-       
-      <h1>Hello</h1>
+       <div className='foodView'>
+            <h1 className='foodHeading'>Food facts and inspiration</h1>
+            <div className='nutritionFacts'>
+                <h2>Nutrition</h2>
+            </div>
+            
+       </div>
+      
     )
 }
 
