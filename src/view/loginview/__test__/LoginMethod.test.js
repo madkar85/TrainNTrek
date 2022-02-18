da096a3c5c90134d219500bab49c75417c147118
madkar85 <madeleine.karlsson@outlook.com>
2022-02-18 09:21:25 +0100

    Added some text in foodview and a test in LoginMethod.test

--- src/view/loginview/__test__/LoginMethod.test.js
+++ src/view/loginview/__test__/LoginMethod.test.js
@@ -1,7 +1,10 @@
 const Register = require("../LoginMethod")
 const cases = [["Kalle", "Kalle123", "Kalle"]];
+const cases = [["Madde", "abc123", "Madde"]];
+const cases = [["", "abc123", ""]];
 
 // Test for testing register method.
+/*
 describe('testing register method', () => {
   test.each(cases)(
     'Test if given username is stored in localstorage.',
@@ -11,4 +14,15 @@ describe('testing register method', () => {
       expect(actual).toEqual(expectedResult)
     }
   )
+})
+*/
+
+
+describe('test for register method', () => {
+    test.each(cases)('Test if name tryin to register is stored', (username, password, expectedResult) => {
+        Register(username, password)
+        const actual = localStorage.getItem('username')
+        expect(actual).toEqual(expectedResult)
+    }
+   )
 })
