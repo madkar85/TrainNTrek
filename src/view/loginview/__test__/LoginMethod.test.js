c0dc341acf167fa1f01ef642d428dfc36ac8b96c
madkar85 <madeleine.karlsson@outlook.com>
2022-02-18 10:57:04 +0100

    Added test documentation

--- src/view/loginview/__test__/LoginMethod.test.js
+++ src/view/loginview/__test__/LoginMethod.test.js
@@ -3,19 +3,6 @@ const cases = [["Kalle", "Kalle123", "Kalle"], ["Madde", "abc123", "Madde"], ["n
 
 
 // Test for testing register method.
-/*
-describe('testing register method', () => {
-  test.each(cases)(
-    'Test if given username is stored in localstorage.',
-    (username, password, expectedResult) => {
-      Register(username, password)
-      const actual = localStorage.getItem('username')
-      expect(actual).toEqual(expectedResult)
-    }
-  )
-})
-*/
-
 
 describe('test for register method', () => {
     test.each(cases)('Test if name tryin to register is stored', (username, password, expectedResult) => {
